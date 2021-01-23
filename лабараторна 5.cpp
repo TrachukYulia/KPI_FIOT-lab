51213351d3f28c3ad85381080512eb652219f017
TrachukYulia <71709949+TrachukYulia@users.noreply.github.com>
2021-01-23 17:01:25 +0200

    Add files via upload

---
+++ лабараторна 5.cpp
@@ -0,0 +1,33 @@
+﻿
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int n, m;
+
+    setlocale(LC_ALL, "Ukr");
+    printf("Трачук Юлiя IС-01\n");
+
+    cout << "Ввести число n = ";  cin >> n; //Введення n
+    if (n > 0 && n < 27)// Умова виконная програми
+    {
+       int sum = 0; // Присвоїти початковій суммі значення 0
+       int m = 0;
+        while (sum == !n)
+        {
+            sum += m % 10;  // Обчислюємо суму 
+            m /= 10;
+         cout << "Сума m останнiх цифер числа n" << " = " << m << endl;
+        }
+ 
+    }
+    else cout << "Неможливо визначити\n";
+
+    return 0;
+    system("pause");
+}
+
+
